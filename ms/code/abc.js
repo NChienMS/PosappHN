99806f1cf0c03f737bebf11c26d47366e163f8c0
NChienMS <97020036+NChienMS@users.noreply.github.com>
2025-10-17 10:37:40 +0700

    Delete ms directory

--- ms/code/abc.js
+++
@@ -1,5 +0,0 @@
-function changeImage(id){
-	let imagePath = document.getElementById(id).getAttribute('src');
-	console.log(imagePath);
-	document.getElementById('img-main').setAttribute('src',imagePath)
-}
